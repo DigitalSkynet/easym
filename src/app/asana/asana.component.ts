1b84369487b8eb13ae851a640d7a3bb9faed893f
Andrey Kurilo <andrey@digitalskynet.com>
2018-03-21 21:42:04 +0300

    Added promises to wait all tasks to load Added check box to toggle visibility of the workspaces with no tasks

--- src/app/asana/asana.component.ts
+++ src/app/asana/asana.component.ts
@@ -28,6 +28,7 @@ export class AsanaComponent implements OnInit {
   workspaces = [];
   loading = false;
   client = null;
+  showEmptyWorkspaces: false;
   oauthData = null;
   oauth = {
     clientId: '598248304930817',
@@ -36,6 +37,10 @@ export class AsanaComponent implements OnInit {
   asanaConnectUrl = `https://app.asana.com/-/oauth_authorize?client_id=${this.oauth.clientId}&redirect_uri=${this.oauth.redirectUrl}&response_type=token`;
 
   that = this;
+
+  isWorkspaceVisible(workspace) {
+    return this.showEmptyWorkspaces || workspace && workspace.tasks && workspace.tasks.length;
+  }
 
   handleRefreshToken() {
     this.router.navigate(['']);
@@ -64,25 +69,49 @@ export class AsanaComponent implements OnInit {
       'assignee': 'me',
       'opt_fields': 'id,name,created_at,completed,projects,tags,due_on,custom_fields'
     };
-    this.client.workspaces.tasks(query, function (errTasks, tasks) {
-      workspace.tasks = tasks.sort((a, b) => that.sortTasks(a, b));
+
+    const tasksPromise = new Promise((resolve, reject) => {
+      this.client.workspaces.tasks(query, function (errTasks, tasks) {
+        if (tasks) {
+          workspace.tasks = tasks.sort((a, b) => that.sortTasks(a, b));
+          resolve();
+        } else {
+          reject();
+        }
+      });
     });
+
+    return tasksPromise;
   }
 
   loadWorspaces() {
     const that = this;
     that.loading = true;
+    const tasksPromises = [];
     this.client.workspaces.list(function (err, workspaces) {
       if (workspaces) {
         for (const workspace of workspaces) {
-          that.loadTasks(workspace);
+          const taskPromise = that.loadTasks(workspace);
+          tasksPromises.push(taskPromise);
         }
-        that.workspaces = workspaces;
+        Promise.all(tasksPromises).then(value => {
+          console.log(value);
+          that.workspaces = workspaces;
+          that.loading = false;
+        }, reason => {
+          that.loading = false;
+          that.handleRefreshToken();
+          console.log(reason);
+        });
+
       } else {
+        that.loading = false;
         that.handleRefreshToken();
+        return;
       }
-      that.loading = false;
     });
+
+
   }
 
   ngOnInit() {
